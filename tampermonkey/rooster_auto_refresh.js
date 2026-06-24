f97fb03864e07bd7a919ca94a7b023840915f873
A3 <59868436+101br03k@users.noreply.github.com>
2026-06-24 10:23:04 +0200

    Update rooster_auto_refresh.js

--- tampermonkey/rooster_auto_refresh.js
+++ tampermonkey/rooster_auto_refresh.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         rooster auto refreshen
-// @namespace    https://rooster-servicedesk.ucit.vu.nl/rooster.htm
-// @version      0.1
+// @namespace    https://vunl.sharepoint.com/sites/Servicedesk/Gedeelde%20documenten/Forms/AllItems.aspx?id=%2Fsites%2FServicedesk%2FGedeelde%20documenten%2FGeneral%2FIT%20Servicedesk%2FRooster%2Frooster%2Ehtm&parent=%2Fsites%2FServicedesk%2FGedeelde%20documenten%2FGeneral%2FIT%20Servicedesk%2FRooster
+// @version      0.2
 // @description  Rooster auto refreshen
 // @author       John de Bever
-// @match        https://rooster-servicedesk.ucit.vu.nl/rooster.htm
+// @match        https://vunl.sharepoint.com/sites/Servicedesk/Gedeelde%20documenten/Forms/AllItems.aspx?id=%2Fsites%2FServicedesk%2FGedeelde%20documenten%2FGeneral%2FIT%20Servicedesk%2FRooster%2Frooster%2Ehtm&parent=%2Fsites%2FServicedesk%2FGedeelde%20documenten%2FGeneral%2FIT%20Servicedesk%2FRooster
 // ==/UserScript==
 
 setTimeout(function(){ window.location.href = window.location.href; }, 500000); // getal is tijd in ms
